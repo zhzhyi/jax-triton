6abd4ba6422eeb526f8c9f48ee658864d355895c
Zhaoyi Zhang <zhaoyi_zhang2@apple.com>
2023-06-30 11:09:05 -0700

    update

--- jax_triton/pallas/ops/attention_benchmark.py
+++ jax_triton/pallas/ops/attention_benchmark.py
@@ -18,7 +18,7 @@ from jax_triton.pallas.ops import attention
 @benchmark.option.args([128, 32, 2048, 32])
 @benchmark.option.args([128, 32, 2048, 64])
 @benchmark.option.iterations(100)
-def TritonMHA(benchmark_state: benchmark.State):
+def TritFWD(benchmark_state: benchmark.State):
   k1, k2, k3 = random.split(random.PRNGKey(0), 3)
 
 
@@ -46,9 +46,9 @@ def TritonMHA(benchmark_state: benchmark.State):
 @benchmark.option.args([128, 16, 2048, 2])
 @benchmark.option.args([128, 32, 2048, 2])
 @benchmark.option.args([128, 32, 2048, 32])
-@benchmark.option.args([128, 32, 2048, 64])
+# @benchmark.option.args([128, 32, 2048, 64]) OOM
 @benchmark.option.iterations(100)
-def BaseLiMHA(benchmark_state: benchmark.State):
+def BaseFWD(benchmark_state: benchmark.State):
   k1, k2, k3 = random.split(random.PRNGKey(0), 3)
 
 
@@ -67,6 +67,72 @@ def BaseLiMHA(benchmark_state: benchmark.State):
 
   while benchmark_state:
     o = f(q, k, v).block_until_ready()
+
+
+@benchmark.register
+@benchmark.option.unit(benchmark.kMillisecond)
+@benchmark.option.args([16, 4, 64, True])
+@benchmark.option.args([128, 2, 2048, 2])
+@benchmark.option.args([128, 8, 2048, 2])
+@benchmark.option.args([128, 16, 2048, 2])
+@benchmark.option.args([128, 32, 2048, 2])
+@benchmark.option.args([128, 32, 2048, 32])
+@benchmark.option.args([128, 32, 2048, 64])
+@benchmark.option.iterations(100)
+def TritBWD(benchmark_state: benchmark.State):
+  k1, k2, k3 = random.split(random.PRNGKey(0), 3)
+
+
+  head_dim = benchmark_state.range(0)
+  num_heads = benchmark_state.range(1)
+  seq_len = benchmark_state.range(2)
+
+  batch_size = benchmark_state.range(3)
+  q = random.normal(k1, (batch_size, seq_len, num_heads, head_dim), dtype=jnp.float16)
+  k = random.normal(k2, (batch_size, seq_len, num_heads, head_dim), dtype=jnp.float16)
+  v = random.normal(k3, (batch_size, seq_len, num_heads, head_dim), dtype=jnp.float16)
+
+  @jax.jit
+  def f(q, k, v):
+    return attention.mha(q, k, v).sum()
+
+  dq, dk, dv = jax.grad(f, argnums=(0, 1, 2))(q, k, v).block_until_ready()
+
+  while benchmark_state:
+    dq, dk, dv = jax.grad(f, argnums=(0, 1, 2))(q, k, v).block_until_ready()
+
+
+@benchmark.register
+@benchmark.option.unit(benchmark.kMillisecond)
+@benchmark.option.args([16, 4, 64, True])
+@benchmark.option.args([128, 2, 2048, 2])
+@benchmark.option.args([128, 8, 2048, 2])
+@benchmark.option.args([128, 16, 2048, 2])
+@benchmark.option.args([128, 32, 2048, 2])
+@benchmark.option.args([128, 32, 2048, 32])
+# @benchmark.option.args([128, 32, 2048, 64]) OOM
+@benchmark.option.iterations(100)
+def BaseBWD(benchmark_state: benchmark.State):
+  k1, k2, k3 = random.split(random.PRNGKey(0), 3)
+
+
+  head_dim = benchmark_state.range(0)
+  num_heads = benchmark_state.range(1)
+  seq_len = benchmark_state.range(2)
+
+  batch_size = benchmark_state.range(3)
+  q = random.normal(k1, (batch_size, seq_len, num_heads, head_dim), dtype=jnp.float16)
+  k = random.normal(k2, (batch_size, seq_len, num_heads, head_dim), dtype=jnp.float16)
+  v = random.normal(k3, (batch_size, seq_len, num_heads, head_dim), dtype=jnp.float16)
+
+  @jax.jit
+  def f_ref(q, k, v):
+    return attention.mha_reference(q, k, v).sum()
+
+  dq_ref, dk_ref, dv_ref = jax.grad(f_ref, argnums=(0, 1, 2))(q, k, v).block_until_ready()
+
+  while benchmark_state:
+    dq_ref, dk_ref, dv_ref = jax.grad(f_ref, argnums=(0, 1, 2))(q, k, v).block_until_ready()
 
 
 if __name__ == "__main__":
